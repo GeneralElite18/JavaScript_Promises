5f0ec2582cf6bd7f273bf1672e15cdd856c3c5e8
Alex Gonzales <gonzales.alexzander18@gmail.com>
2023-07-10 08:56:06 -0600

    finished exercise

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
  *
  * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
  */
+let error = document.getElementById("error");
+let theList = document.getElementById("list");
+
 function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -16,7 +19,22 @@ function getList() {
 }
 
 // TODO: Handle the resolved or rejected states of the promise
+getList()
+.then((list) => {
+  console.log(list);
+  for(let i = 0; i < list.length; i++){
+    let newItem = document.createElement("li");
+    newItem.textContent = list[i];
+    theList.append(newItem);
+  }
 
+})
+.catch((err) => {
+  console.error(err);
+  let newError = document.createElement("p");
+  newError.textContent = err;
+  error.append(newError);
+})
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
 
